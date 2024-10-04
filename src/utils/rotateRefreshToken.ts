f874010825e99384c02d41da89be2f9b6784f626
bilal <bilalaney084@gmail.com>
2024-10-04 20:21:38 +0300

    feature - Adding a security layer in refresh token logic

--- src/utils/rotateRefreshToken.ts
+++ src/utils/rotateRefreshToken.ts
@@ -7,13 +7,16 @@ export default async function rotateRefreshToken(
   newToken: string
 ): Promise<void> {
   try {
-    await refreshTokenModel.findOneAndDelete({ userId });
+    const deletedToken = await refreshTokenModel.findOneAndDelete({ userId });
+    if (!deletedToken)
+      throw new Error("Rotation Error | No previous refresh token");
     await refreshTokenModel.create({
       userId,
       refreshToken: newToken,
       state: "active",
     });
   } catch (e) {
-    throw new Error("Rotation Error | Failed to rotate the refresh token");
+    if (e instanceof Error) console.log("Error | Rotation Error \n", e.message);
+    // throw new Error("Rotation Error | Failed to rotate the refresh token");
   }
 }
